2f66186908fd0205a92cc61540d7c4689cea22f4
Joseph Senders <jasenders@gmail.com>
2018-08-08 20:53:20 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,10 +4,10 @@ def roll_call_dwarves(dwarves)
   end
 end
 
-
-
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planateers)
+  planateers.map do |planateer|
+    planateer.capitalize + "!"
+end
 end
 
 def long_planeteer_calls# code an argument here
