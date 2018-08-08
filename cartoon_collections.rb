a8376eb7a8eb6d26d1e3d941f674ef585f853520
Joseph Senders <jasenders@gmail.com>
2018-08-08 20:50:03 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,6 +2,8 @@ def roll_call_dwarves(dwarves)# code an argument here
 dwarves.each_with_index do |dwarf , index|
   puts /(index+1).(dwarf)/
 end
+end
+
 
 def summon_captain_planet# code an argument here
   # Your code here
